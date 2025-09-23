86f7e10efec93c349f47a1338944ebc3ad934876
Daniel Delgado <ddelgadopi@unal.edu.co>
2025-09-22 22:16:04 -0500

    Corrigiendo app.py para correr el pytest

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from src.models import Base
 from src.database import engine
 from src.routes.users_routes import router as user_router
 
-# Crear las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
 # Registrar rutas
 app.include_router(user_router, prefix="/users", tags=["Users"])
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
